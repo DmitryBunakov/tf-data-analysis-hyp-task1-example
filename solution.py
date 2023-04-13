11c8c1b503c835da43083d0e9e106441151e6d68
DmitryBunakov <92208060+DmitryBunakov@users.noreply.github.com>
2023-04-13 23:56:20 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -14,22 +14,7 @@ def solution(x_success: int,
     
     alpha = 0.07
     
-    # Вычисляем доли продаж
-    p1 = x_success / x_cnt
-    p2 = y_success / y_cnt
-   
-    # Вычисляем стандартную ошибку разности долей
-    P = (x_success + y_success) / (x_cnt + y_cnt)
-    SE = np.sqrt(P*(1-P)*(1/x_cnt + 1/y_cnt))
-
-    # Вычисляем z-статистику
-    z = (p2 - p1) / SE
-
-    # Вычисляем критическое значение z-статистики
-    z_crit = norm.ppf(1-alpha/2)
-
-    # Сравниваем с критическим значением z_crit
-    if z > z_crit:
-        return True
-    else:
-        return False
+    count = np.array([x_success, y_success])
+    nobs = np.array([x_cnt, y_cnt])
+    zstat, p_value = proportions_ztest(count=count, nobs=nobs, alternative='smaller')
+    return p_value < alpha
